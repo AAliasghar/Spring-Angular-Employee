c6cfca1d891e8bde9d41cff513dd7fb220d65b6d
AAliasghar <a.asgharinia@yahoo.com>
2023-02-20 00:05:56 +0100

    first commit

---
+++ 02-Backend/spring-boot-employee/spring-boot-employee/src/main/java/com/ali/springbootemployee/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.ali.springbootemployee.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ali.springbootemployee.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+    
+}
