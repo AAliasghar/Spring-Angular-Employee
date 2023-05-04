c31c386149d7e5844ce6ece3fc60e09c490d2f6e
AAliasghar <a.asgharinia@yahoo.com>
2023-05-04 22:35:06 +0200

    REST API Documentation

--- 02-Backend/employee/src/main/java/com/springbootbackend/employee/EmployeeApplication.java
+++ 02-Backend/employee/src/main/java/com/springbootbackend/employee/EmployeeApplication.java
@@ -5,11 +5,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(title = "Spring Boot API Documentation",
+ 					description = "Spring Boot API Documentation", version = "v1.0",
+  					contact = @Contact(name = "AAliasghar", 
+  					url = "https://github.com/AAliasghar/Spring-Angular-Employee")
+  ),
+  	 externalDocs = @ExternalDocumentation(description = "Spring Boot Employee Management Documentation",
+   										 url = "https://github.com/AAliasghar/Spring-Angular-Employee"))
 public class EmployeeApplication {
 
 	@Bean
-	public ModelMapper modelMapper(){
+	public ModelMapper modelMapper() {
 		return new ModelMapper();
 	}
 
